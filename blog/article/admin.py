12fbeaa072e2ad8eef94b586e7cbe436e381e61d
Dera <ugonuchidera555@gmail.com>
2023-10-02 17:31:27 +0100

    admin

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+@admin.register(Comment)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'created_date']
+    
+    list_display_links = ['title', 'created_date']
+    
+    search_fields = ['title']
+    
+    list_filter = ['created_date']
+    
+    prepopulated_fields = {'slug':('title',)}
--- blog/article/models.py
+++ blog/article/models.py
@@ -18,3 +18,15 @@ class Article(models.Model):
         if not self.slug:
             self.slug = Slug(self.title)
         return super(Article, self).save(*args, **kwargs)
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name="Article", related_name="comments")
+    comment_author = models.CharField(max_length=100, verbose_name="Commenter")
+    comment_content = models.CharField(max_length=200, verbose_name="Comment")
+    comment_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.comment_content
+    
+    class Meta:
+        ordering = ['-comment_date']
